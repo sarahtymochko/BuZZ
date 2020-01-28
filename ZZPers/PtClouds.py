4b9c1ef8af8a7acba4759532441157f7810ddc88
Sarah Tymochko <tymochko@egr.msu.edu>
2020-01-28 17:28:30 -0500

    Added changing radius feature... still debugging

--- ZZPers/PtClouds.py
+++ ZZPers/PtClouds.py
@@ -7,7 +7,7 @@ from ZZPers.ZZPers.PD import PD
 import time
 from ripser import ripser
 
-## Using a fixed radius is working... Need to add changing radius 
+## Using a fixed radius is working... Need to add changing radius
 
 class PtClouds(object):
     '''
@@ -72,8 +72,12 @@ class PtClouds(object):
         self.r = r
         self.k = k
 
+
         ft_st = time.time()
-        filtration, times = self.setup_Zigzag(r = self.r, k = self.k, verbose = self.verbose)
+        if type(r) == float or type(r) == int:
+            filtration, times = self.setup_Zigzag(r = self.r, k = self.k, verbose = self.verbose)
+        else:
+            filtration, times = self.setup_Zigzag_LIST(r = self.r, k = self.k, verbose = self.verbose)
         ft_end = time.time()
 
         self.filtration = filtration
@@ -155,7 +159,7 @@ class PtClouds(object):
 
         lst = list(self.ptclouds['PtCloud'])
 
-        simps_df = pd.DataFrame(columns = ['Simp','B,D'])
+        # simps_df = pd.DataFrame(columns = ['Simp','B,D'])
         simps_list = []
         times_list = []
 
@@ -242,6 +246,8 @@ class PtClouds(object):
                     times_list.append([i-0.5,i])
                     M.add(simp)
 
+
+
             # Reinitialize for next iteration
             verts = verts_next
             A = B
@@ -249,6 +255,180 @@ class PtClouds(object):
         loop_end = time.time()
         if verbose:
             print(f'Loop done in {loop_end-loop_st} seconds...')
+
+        f_st = time.time()
+        filtration = dio.Filtration(simps_list)
+        f_end = time.time()
+
+        if verbose:
+            print(f'Calculated filtration in {f_end-f_st} seconds...\n')
+
+        return filtration, times_list
+
+
+    def setup_Zigzag_LIST(self, r, k=2, verbose=False):
+        '''
+        Helper function for ``run_Zigzag`` that sets up inputs needed for Dionysus' zigzag persistence function.
+
+        Parameters
+        ----------
+
+        r: float
+            List of radii for rips complex of each X_i
+
+        k: int, optional
+            Max dimension for rips complex (default is 2)
+
+        verbose: bool, optional
+            If true, prints updates when running code
+
+        Returns
+        -------
+        filtration: dio.Filtration
+            Dionysis filtration containing all simplices that exist in zigzag sequence
+
+        times: list
+            List of times, where times[i] is a list containing [a,b] where simplex i appears at time a, and disappears at time b
+
+        '''
+
+        lst = list(self.ptclouds['PtCloud'])
+
+        # simps_df = pd.DataFrame(columns = ['Simp','B,D'])
+        simps_list = []
+        times_list = []
+
+        # Vertex counter
+        vertind = 0
+
+        init_st = time.time()
+        # Initialize A with R(X_0)
+        rips = dio.fill_rips(lst[0], k=2, r=r[0])
+        rips.sort()
+        rips_set = set(rips)
+
+        # Add all simps to the list with birth,death=[0,1]
+        for s in rips_set:
+            # s.data = 0
+            simps_list.append(s)
+            times_list.append([0,1])
+
+        # Initialize with vertices for X_0
+        verts = set([dio.Simplex([j+vertind],0) for j,pc in enumerate(lst[0])])
+
+        # Initialize A with set of simplices with verts in X_0
+        A = rips_set
+
+        init_end = time.time()
+        if verbose:
+            print(f'Initializing done in {init_end-init_st} seconds...')
+
+        loop_st = time.time()
+        # Loop over the rest of the point clouds
+        for i in range(1,len(lst)):
+
+            # Calculate rips of X_{i-1} \cup X_i
+            rips = dio.fill_rips(np.vstack([lst[i-1],lst[i]]), k=2, r=max(r[i-1],r[i]))
+
+            # Adjust vertex numbers, sort, and make into a set
+            rips = fix_dio_vert_nums(rips, vertind)
+            rips.sort()
+            rips_set = set(rips)
+
+            # Increment vertex counter
+            vertind = vertind+len(verts)
+
+            # Set of vertices in R(X_i)
+            verts_next = set([dio.Simplex([j+vertind],0) for j,pc in enumerate(lst[i])])
+
+            # Set of simplices with verts in X_{i}
+            B = set(verts_next.intersection(rips_set))
+
+            # Set of simplices with verts in X_{i-1} AND X_{i}
+            M = set()
+
+            # Loop over vertices in R(X_{i-1} \cup R_i)
+            for simp in rips:
+
+                # Get list of vertices of simp
+                bdy = getVerts(simp) #set([s for s in simp.boundary()])
+
+                # If it has no boundary and its in B, its a vertex we haven't seen yet
+                # So add it to the list with appropriate birth,death
+                if not bdy:
+                    if simp in B:
+                        simps_list.append(simp)
+                        times_list.append([i-0.5,i+1])
+                    continue
+
+                # If all of its verts are in A, it's been handled in the initialization or the previous iteration
+                if bdy.intersection(A) == bdy:
+                    if r[i-1] < r[i]:
+                        if simp.data > r[i-1]:
+                            # If we haven't seen it before, add it to the list with appropriate birth,death times
+                            if simp not in simps_list:
+                                simps_list.append(simp)
+                                times_list.append([i-0.5,i])
+                                M.add(simp)
+                            # If we've already added it to the list...
+                            else:
+                                # Find where it is in the list
+                                simp_ind = simps_list.index(simp)
+
+                                # Remove the simplex and its birth,death time from each list
+                                simps_list.pop(simp_ind)
+                                curr_times = times_list.pop(simp_ind)
+
+                                # Readd to lists and M
+                                # Concatenate birth,death times we've added before with new ones
+                                simps_list.append(simp)
+                                times_list.append(curr_times + [i-0.5,i])
+                                M.add(simp)
+
+
+                # If all of its verts are in B, it exists in B and in the following union
+                # Add it to the list with appropriate birth,death
+                elif bdy.intersection(B) == bdy:
+
+                    # If r[i-1] <= r[i], anything with verts in B should also be in B
+                    if r[i-1] <= r[i]:
+                    # simp.data = 0
+                        simps_list.append(simp)
+                        times_list.append([i-0.5,i+1])
+                        B.add(simp)
+
+                    # If r[i-1] > r[i], we need to check if it should go in M or B
+                    else:
+                        # If simplex's birth timee is greater than the radius, it goes in M
+                        if simp.data > r[i]:
+                            simps_list.append(simp)
+                            times_list.append([i-0.5,i])
+                            M.add(simp)
+
+                        # If it's <= then it goes in B
+                        else:
+                            simps_list.append(simp)
+                            times_list.append([i-0.5,i+1])
+                            B.add(simp)
+
+                # If it has some verts in A and some in B, it only exists in the union
+                # Add to list and set birth,death appropriately
+                else:
+
+                    # simp.data = 0
+                    simps_list.append(simp)
+                    times_list.append([i-0.5,i])
+                    M.add(simp)
+
+
+            # Reinitialize for next iteration
+            verts = verts_next
+            A = B
+
+        loop_end = time.time()
+        if verbose:
+            print(f'Loop done in {loop_end-loop_st} seconds...')
+
 
         f_st = time.time()
         filtration = dio.Filtration(simps_list)
@@ -306,11 +486,11 @@ def fix_dio_vert_nums(Cplx, vertind, data=None):
         vlist = []
         for v in s:
             vlist.append(v+vertind)
-        s = dio.Simplex(vlist)
-        if data:
-            s.data = data
-        else:
-            s.data = 0
+        s = dio.Simplex(vlist,s.data)
+        # if data:
+        #     s.data = data
+        # else:
+        #     s.data = 0
         # print(s)
         New_Cplx.append(s)
     return New_Cplx
